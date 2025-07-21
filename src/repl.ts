80c7af74c373419d54ed722795d61d8ab859e259
AcharaChisomSolomon <acharachisom861@gmail.com>
2025-07-22 00:47:03 +0100

    help and exit commands handled

--- src/repl.ts
+++ src/repl.ts
@@ -1,23 +1,33 @@
-import { createInterface } from 'node:readline';
+import { State } from "./state.js";
 
-export function startREPL() {
-  const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: "Pokedex > "
-  });
-  rl.prompt();
+export function startREPL(state: State) {
+  state.readline.prompt();
 
-  rl.on("line", async (input) => {
+  state.readline.on("line", async (input) => {
     const words = cleanInput(input);
     if (words.length === 0) {
-      rl.prompt();
+      state.readline.prompt();
       return;
     }
 
     const commandName = words[0];
-    console.log(`Your command was: ${commandName}`);
-    rl.prompt();
+
+    const cmd = state.commands[commandName];
+    if (!cmd) {
+      console.log(
+        `Unknown command: "${commandName}". Type "help" for a list of commands.`,
+      );
+      state.readline.prompt();
+      return;
+    }
+
+    try {
+      cmd.callback(state);
+    } catch (e) {
+      console.log(e);
+    }
+
+    state.readline.prompt();
   });
 }
 
